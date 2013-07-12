ae96bb8f9f85e2923d7206314415f8fd06916b32
sneha-thangaraja <sneha.thangaraja@gmail.com>
2013-07-12 18:44:03 +0000

    web.js edit 4

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
 var buffer = new Buffer(40);
+var in = fs.readFileSync(index.html);
+var out = buffer.toString(in);
+var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString(fs.readFileSync(index.html)));
+  response.send(buffer.toString(out);
 });
 
 var port = process.env.PORT || 5000;
